3872f3f0551ec571acfae1dd7506028cefdfbec5
Hiroto_Kikko <a240371h@reitaku.jp>
2026-02-03 00:09:28 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function greet() {
   console.log("Hello from the main branch!");
 }
+=======
+function greet() {
+  console.log("Hello from the local branch!");
+}
+>>>>>>> 1f6fffd (私たちの修正)
